cad48d46d9ce95c31b6251ed1617de3a7de7c3c5
Philipp Gmür <p>
2023-02-19 20:31:09 +0100

    settings.js

--- client/src/settings.js
+++ client/src/settings.js
@@ -1,3 +1,3 @@
 // Konstanten für das Frontend.
-export const Server = "http://localhost:5000/"
+export const Server = "http://localhost:5000/";  // Anpassen beim deployen.
 export const RefreshIntervalSeconds = 30;
